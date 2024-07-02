06eaea4acc9aff058c71e2b9b62c8b61782c3f2d
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-02 13:26:57 +0300

    0.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
-import tasks.AnyStringTask;
+import tasks.stringTasks.StringReverser;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(AnyStringTask.joinIn("!", "один", "два", "три", "четыре", "пять"));
-        System.out.println(AnyStringTask.repeater("точно ", 10));
-        System.out.println(AnyStringTask.reverser("отунревереп"));
-        System.out.println(AnyStringTask.trueOrFalse("888888888888888B88888888888"));
-        System.out.println(AnyStringTask.trueOrFalse("888888888888888888888888888"));
+        System.out.println(StringReverser.reverser("курага"));
     }
 }
---
+++ src/main/java/tasks/stringTasks/StringReverser.java
@@ -0,0 +1,19 @@
+package tasks.stringTasks;
+
+public class StringReverser {
+
+    //со стрингбилдером
+    public static StringBuilder reverseByBuilder(String text) {
+        return new StringBuilder(text).reverse();
+    }
+    //без него
+    public static String reverser(String text) {
+        String reverseText = "";
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            reverseText = symbol + reverseText;
+        }
+
+        return reverseText;
+    }
+}
