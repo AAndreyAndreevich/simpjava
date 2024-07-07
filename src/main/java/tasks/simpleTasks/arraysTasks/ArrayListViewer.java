e8245902c5e814d82256792c08f86cd108111f60
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-08 02:43:04 +0300

    0.5

--- src/main/java/tasks/simpleTasks/arraysTasks/ArrayListViewer.java
+++ src/main/java/tasks/simpleTasks/arraysTasks/ArrayListViewer.java
@@ -1,27 +1,17 @@
 package tasks.simpleTasks.arraysTasks;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListViewer {
 
-    static List<String> someList = new ArrayList<>();
-    static {
-        someList.add("Ноль");
-        someList.add("Один");
-        someList.add("Два");
-        someList.add("Три");
-        someList.add("Четыре");
-    }
-
-    public static void forIArray() {
+    public static void forIArray(List<String> someList) {
         System.out.println("for (инициализация;условие;итерация)");
         for (int i = 0; i < someList.size(); i++) {
             System.out.println(someList.get(i));
         }
     }
 
-    public static void whileArray() {
+    public static void whileArray(List<String> someList) {
         System.out.println("while (условие)");
         int i = 0;
         while (i < someList.size()) {
@@ -30,14 +20,10 @@ public class ArrayListViewer {
         }
     }
 
-    public static void forArray() {
+    public static void forArray(List<String> someList) {
         System.out.println("for (инициализация;список)");
         for (String s : someList) {
             System.out.println(s);
         }
-    }
-
-    public static List<String> getSomeList() {
-        return someList;
     }
 }
--- src/test/java/tasks/simpleTasks/arraysTasks/ArrayListViewerTest.java
+++ src/test/java/tasks/simpleTasks/arraysTasks/ArrayListViewerTest.java
@@ -1,31 +1,34 @@
 package tasks.simpleTasks.arraysTasks;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayListViewerTest {
 
+    static final List<String> testList = new ArrayList<>();
+
+    static {
+        testList.add("Ноль");
+        testList.add("Один");
+        testList.add("Два");
+        testList.add("Три");
+        testList.add("Четыре");
+    }
+
     @Test
     void forIArray() {
-
+        ArrayListViewer.forIArray(testList);
     }
 
     @Test
     void whileArray() {
-
+        ArrayListViewer.whileArray(testList);
     }
 
     @Test
     void forArray() {
-
-    }
-
-    @Test
-    void getSomeList() {
-
+        ArrayListViewer.forArray(testList);
     }
 }
