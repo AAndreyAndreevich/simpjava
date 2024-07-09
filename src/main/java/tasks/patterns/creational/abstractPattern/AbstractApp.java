1d0109b0bce6904d85fc054c9d9cd55f91c6291f
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-09 18:59:36 +0300

    0.5

---
+++ src/main/java/tasks/patterns/creational/abstractPattern/AbstractApp.java
@@ -0,0 +1,4 @@
+package tasks.patterns.creational.abstractPattern;
+
+public class AbstractApp {
+}
---
+++ src/main/java/tasks/patterns/creational/factoryPattern/FactoryApp.java
@@ -0,0 +1,54 @@
+package tasks.patterns.creational.factoryPattern;
+
+public class FactoryApp {
+
+    public static void main(String[] args) {
+        CreatorProduct creator = createProduct(1);
+        Product product = creator.newProduct();
+        product.someMethod();
+    }
+
+    interface Product {
+        void someMethod();
+    }
+
+    static class ConcreteProduct1 implements Product {
+        @Override
+        public void someMethod() {
+            System.out.println("method Concrete Product 1");
+        }
+    }
+
+    static class ConcreteProduct2 implements Product {
+        @Override
+        public void someMethod() {
+            System.out.println("method Concrete Product 2");
+        }
+    }
+
+    interface CreatorProduct {
+        Product newProduct();
+    }
+
+    static class CreatorConcreteProduct1 implements CreatorProduct {
+        @Override
+        public Product newProduct() {
+            return new ConcreteProduct1();
+        }
+    }
+
+    static class CreatorConcreteProduct2 implements CreatorProduct {
+        @Override
+        public Product newProduct() {
+            return new ConcreteProduct2();
+        }
+    }
+
+    static CreatorProduct createProduct(int v) {
+        switch (v) {
+            case 1 : return new CreatorConcreteProduct1();
+            case 2 : return new CreatorConcreteProduct2();
+            default : throw new RuntimeException("");
+        }
+    }
+}
