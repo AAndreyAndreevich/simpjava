bea4b48b972adf9357de41e516e8ef18353223b3
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-10 19:15:36 +0300

    0.5

---
+++ src/main/java/tasks/patterns/creational/PrototypeApp.java
@@ -0,0 +1,61 @@
+package tasks.patterns.creational;
+
+public class PrototypeApp {
+
+    public static void main(String[] args) {
+        Product product = new Product("one", 1L);
+        System.out.println(product);
+        Product product1 = (Product) product.getClone();
+        System.out.println(product1);
+        product.setName("null");
+        product.setNumber(0L);
+        System.out.println(product);
+    }
+
+    interface Prototype {
+        Object getClone();
+    }
+
+    static class Product implements Prototype {
+
+        private String name;
+        private Long number;
+
+        public Product(String name, Long number) {
+            this.name = name;
+            this.number = number;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Product setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Long getNumber() {
+            return number;
+        }
+
+        public Product setNumber(Long number) {
+            this.number = number;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Product{" +
+                    "name='" + name + '\'' +
+                    ", number=" + number +
+                    '}';
+        }
+
+        @Override
+        public Object getClone() {
+            return new Product(name, number);
+        }
+    }
+
+}
