77a39b20d8d9c00b84e2909701d3e5abcd15b9ed
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-11 17:54:35 +0300

    0.5

--- src/main/java/tasks/patterns/structural/DecoratorApp.java
+++ src/main/java/tasks/patterns/structural/DecoratorApp.java
@@ -4,8 +4,49 @@ public class DecoratorApp {
 
     public static void main(String[] args) {
 
+        ConcreteDecorator concreteDecorator = new ConcreteDecorator(new ConcreteComponent());
+        concreteDecorator.superOperation();
+
     }
 
+    interface Component {
+        void operation();
+    }
 
+    static class ConcreteComponent implements Component {
+        @Override
+        public void operation() {
+            System.out.println("Concrete Component operation");
+        }
+    }
+
+    static class Decorator implements Component {
+
+        Component component;
+        Decorator(Component component) {
+            this.component = component;
+        }
+
+        @Override
+        public void operation() {
+            System.out.println("Decorator operation");
+        }
+    }
+
+    static class ConcreteDecorator extends Decorator {
+        ConcreteDecorator(Component component) {
+            super(component);
+        }
+
+        public void superOperation() {
+            System.out.println("Super operation Concrete Decorator");
+        }
+
+        @Override
+        public void operation() {
+            super.operation();
+            superOperation();
+        }
+    }
 
 }
