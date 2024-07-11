42d69775359af6eb0631020a203ca15453ebd5ac
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-11 16:17:05 +0300

    0.5

---
+++ src/main/java/tasks/patterns/structural/BridgeApp.java
@@ -0,0 +1,62 @@
+package tasks.patterns.structural;
+
+public class BridgeApp {
+
+    public static void main(String[] args) {
+        Abstraction rfa = new RefinedAbstraction(new ConcreteImplementor2());
+        rfa.absMethod();
+        rfa.withImpl();
+        rfa = new RefinedAbstraction(new ConcreteImplementor1());
+        rfa.absMethod();
+        rfa.withImpl();
+    }
+
+    interface Implementor {
+        void someImplMethod();
+    }
+
+    static abstract class Abstraction {
+        private Implementor implementor;
+
+        Abstraction(Implementor implementor) {
+            this.implementor = implementor;
+        }
+
+        public void setImplementor(Implementor implementor) {
+            this.implementor = implementor;
+        }
+
+        public void withImpl() {
+            implementor.someImplMethod();
+        }
+
+        abstract void absMethod();
+    }
+
+    static class RefinedAbstraction extends Abstraction {
+
+        RefinedAbstraction(Implementor implementor) {
+            super(implementor);
+        }
+
+        @Override
+        void absMethod() {
+            System.out.println("Refined message");
+        }
+    }
+
+    static class ConcreteImplementor1 implements Implementor {
+        @Override
+        public void someImplMethod() {
+            System.out.println("Concrete message 1");
+        }
+    }
+
+    static class ConcreteImplementor2 implements Implementor {
+        @Override
+        public void someImplMethod() {
+            System.out.println("Concrete message 2");
+        }
+    }
+
+}
