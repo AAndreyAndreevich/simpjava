6b23c16460681a0bb4d53978d5fa7ea431716e53
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-19 23:56:01 +0300

    0.5

--- src/main/java/tasks/patterns/behavioral/TemplateMethodApp.java
+++ src/main/java/tasks/patterns/behavioral/TemplateMethodApp.java
@@ -3,9 +3,53 @@ package tasks.patterns.behavioral;
 public class TemplateMethodApp {
 
     public static void main(String[] args) {
+        DocWork docWork = new CSV();
+        docWork.handleDoc();
+        System.out.println("===");
+        docWork = new PDF();
+        docWork.handleDoc();
+    }
+
+    static abstract class DocWork {
+
+        public void handleDoc() {
+            loadDoc();
+            rework();
+            exit();
+        }
+
+        void loadDoc() {
+            System.out.println("Document loading");
+        }
+
+        abstract void rework();
+        abstract void exit();
 
     }
 
-    
+    static class PDF extends DocWork {
+
+        @Override
+        void rework() {
+            System.out.println("Обработка PDF");
+        }
+
+        @Override
+        void exit() {
+            System.out.println("Конец PDF");
+        }
+    }
+
+    static class CSV extends DocWork {
+        @Override
+        void rework() {
+            System.out.println("Обработка CSV");
+        }
+
+        @Override
+        void exit() {
+            System.out.println("Конец CSV");
+        }
+    }
 
 }
