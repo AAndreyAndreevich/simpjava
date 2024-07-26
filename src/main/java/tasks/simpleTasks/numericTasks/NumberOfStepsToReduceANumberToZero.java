4d786e87606bff3ff6e2781ef005243f6b503128
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-07-26 15:05:15 +0300

    !0

--- src/main/java/tasks/simpleTasks/numericTasks/NumberOfStepsToReduceANumberToZero.java
+++ src/main/java/tasks/simpleTasks/numericTasks/NumberOfStepsToReduceANumberToZero.java
@@ -3,14 +3,25 @@ package tasks.simpleTasks.numericTasks;
 public class NumberOfStepsToReduceANumberToZero {
 
     public static void main(String[] args) {
-
+        NumberOfStepsToReduceANumberToZero.numberOfSteps(9);
     }
 
-    public int numberOfSteps(int num) {
-
-
-
-        return num;
+    public static int numberOfSteps(int num) {
+        int count = 0;
+        int tempNum = num;
+        while (num > 0) {
+            count++;
+            if (num % 2 == 0) {
+                tempNum /= 2;
+                System.out.printf("Шаг %d) %d четное; делим на 2 и получаем %d\n", count, num, tempNum);
+                num = tempNum;
+            } else {
+                tempNum -= 1;
+                System.out.printf("Шаг %d) %d не четное; отнимаем 1 и получаем %d\n", count, num, tempNum);
+                num = tempNum;
+            }
+        }
+        return count;
     }
 
 }
